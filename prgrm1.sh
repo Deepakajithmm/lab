57a096180339d91b6bae11d59dedf0819b46a5dd
Adhithyarajan <adhiadhithya39@gmail.com>
2022-05-05 16:56:39 +0530

    prgrm1added

---
+++ prgrm1.sh
@@ -0,0 +1,11 @@
+echo " ARITHMETIC OPERATIONS "
+echo " Enter 2 numbers "
+read a b 
+s=`expr $a + $b`
+su=`expr $a - $b`
+p=`expr $a \* $b`
+d=`expr $a / $b`
+echo " Sum is $s "
+echo " Remainder is $su "
+echo " product is $p "
+echo " Quotient is $d "
